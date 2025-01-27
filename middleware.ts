602f1beb742f88d4bb99d07a37a4f5c04ea16740
RamonsArchive <rmcdeem.m@gmail.com>
2025-01-27 12:01:49 -0800

    made '/api/webhooks/stripe', // Public

--- middleware.ts
+++ middleware.ts
@@ -6,10 +6,14 @@ console.log('This is the middleware config');
 
 export const config = {
   matcher: [
+    '/',
+    '/api/webhooks(.*)', // todo link this proplery. Public
+    '/api/webhooks/stripe', // Public
+
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
-    '/api/webhooks(.*)',
+    
   ],
 }; 
