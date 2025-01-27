d168a3dc8532cae0cd8a249d55c138bdc73e82ca
RamonsArchive <rmcdeem.m@gmail.com>
2025-01-27 12:38:27 -0800

    changed dbname back to imaginify

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -10,6 +10,7 @@ import { createUser, deleteUser, updateUser } from "@/lib/actions/actions";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  console.log(`Webhook is being called ${WEBHOOK_SECRET}`);
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
