e5fcb33d137a5eb9d1c1df69c055c6c86a37dcbb
RamonsArchive <rmcdeem.m@gmail.com>
2025-01-27 12:49:23 -0800

    Changed api routes to public explicitly

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', "/api/:path*", '/api/webhooks(.*)','/api/webhooks/stripe']);
+ export default clerkMiddleware(async (auth, request) => {
+ if(!isPublicRoute(request)) {
+    await auth.protect();
+ }
+}
+);
+
 console.log('This is the middleware config');
 
 
